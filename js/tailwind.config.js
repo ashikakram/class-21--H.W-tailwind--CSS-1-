800477d2d1e12a608c920b55b0375c1b7c8d3117
AshikAkram <ashikakram1639@gmail.com>
2024-08-21 05:25:18 +0600

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,31 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // customize start-----------------------
+        maxWidth: {
+          'container': '1140px',
+          },
+        colors: {
+          'container_item':'#FFF3E3',
+          'h1':'#B88E2F',
+          'btn':'#B88E2F',
+          'nav_color':'#ffffff',
+        },
+        backgroundImage: {
+            'banner_img': "url('./images/banner_img1.jpg')",
+            
+          },
+          height: {
+            'con-ite-hei': '290px',
+          },
+          width: {
+            'con-ite-wid': '450px',
+          },
+          fontSize: {
+            
+          },
+        
+        // customize end-----------------------
+      }
+    }
+  }
